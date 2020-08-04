732380504432e35c2a3d4481bb21a9dfb80ea09f
xyz <xyzzhybz@163.com>
2020-08-04 21:15:11 +0800

    finished mock

---
+++ src/test/java/mockito/SecurityCenterTest.java
@@ -0,0 +1,29 @@
+package mockito;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
+
+public class SecurityCenterTest {
+    /* 需求描述：
+    编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
+    * */
+
+    private DoorPanel doorPanel;
+    private SecurityCenter securityCenter;
+
+    @BeforeEach
+    public void setUp() {
+        doorPanel = mock(DoorPanel.class);
+        securityCenter = new SecurityCenter(doorPanel);
+    }
+
+    @Test
+    public void shouldVerifyDoorIsClosed() {
+        securityCenter.switchOn();
+        verify(doorPanel).close();
+    }
+}
